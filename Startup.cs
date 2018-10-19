4e67dfe1e3aa678dcc6b8bb0da3d5acb4211e6bb
Xsitsu <jacoblocke21@live.com>
2018-10-18 17:58:06 -0700

    Fixed startup.cs

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Assignment2
+namespace Lab4
 {
     public class Startup
     {
