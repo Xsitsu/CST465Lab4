a00671b6d9aa40e90f25fdfbf3e435bc3891cb15
Xsitsu <jacoblocke21@live.com>
2018-10-18 20:42:18 -0700

    Added validation to ComputerModel.

--- Models/ComputerModel.cs
+++ Models/ComputerModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,17 +10,28 @@ namespace Lab4.Models
     public class ComputerModel
     {
         [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+
         [Required]
+        [DisplayName("Password")]
         public string AdminPassword { get; set; }
-        [Required]
+
+        [DisplayName("IP Address")]
+        [RegularExpression(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", ErrorMessage = "You must enter a valid IP Address!")]
         public string IpAddress { get; set; }
-        [Required]
+
         public string Location { get; set; }
+
         [Required]
+        [UIHint("OSDropdown")]
         public string OS { get; set; }
+
+        [DisplayName("textarea")]
         public string Description { get; set; }
+
         [Required]
+        [DisplayName("Supported Monitors")]
         [Range(1, 4)]
         public int SupportedMonitors { get; set; }
 
