8d1d1de143f67bf7576da767ddc42c5f11f62f77
Xsitsu <jacoblocke21@live.com>
2018-10-18 18:08:49 -0700

    Added ComputerModel and started adding StandardController.

---
+++ Controllers/StandardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class StandardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ComputerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4.Models
+{
+    public class ComputerModel
+    {
+        public string Name { get; set; }
+        public string AdminPassword { get; set; }
+        public string IpAddress { get; set; }
+        public string Location { get; set; }
+        public string OS { get; set; }
+        public string Description { get; set; }
+        public int SupportedMonitors { get; set; }
+
+        static public List<string> OSTypes = new List<string>() { "iOS", "Windows", "Linux" };
+    }
+}
